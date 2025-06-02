84e225c78b575dcfddd086ba0fa4e3c7b12f82e3
kuriousdesign <gardner.761@gmail.com>
2025-06-02 03:29:30 -0700

    pickId venmo

--- src/components/VenmoLink.tsx
+++ src/components/VenmoLink.tsx
@@ -4,7 +4,7 @@ const payToVenmoAccount = "gardner761";
 export default function VenmoLink({ pickId, amount }: { pickId: string, amount: number}) {
     return(
         <a
-            href={`https://venmo.com/?txn=pay&audience=private&recipients=${payToVenmoAccount}&amount=${amount}&note=${encodeURIComponent(`{hardCharger: ${pickId}}`)}`}
+            href={`https://venmo.com/?txn=pay&audience=private&recipients=${payToVenmoAccount}&amount=${amount}&note=${encodeURIComponent(`{pickId: ${pickId}}`)}`}
             target="_blank"
             rel="noopener noreferrer"
             className="bg-blue-500 text-white px-4 py-2 rounded-full hover:bg-blue-600 flex flex-row items-center justify-center transition-colors duration-300 shadow-md w-fit min-w-[150px]"
