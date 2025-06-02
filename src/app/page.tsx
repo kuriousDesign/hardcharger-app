08732d1d9d6d41da07b3c9aa3b0ecd3397f60b33
kuriousdesign <gardner.761@gmail.com>
2025-06-02 04:26:37 -0700

    another update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,26 @@ import { connectToDatabase } from '@/actions/action';
 import VenmoLink from '@/components/VenmoLink';
 import Link from 'next/link';
 
-import { auth } from '@clerk/nextjs/server';
+import { auth, currentUser } from '@clerk/nextjs/server';
 
 
 export default async function Home() {
 	await connectToDatabase();
+  	// Get the userId from auth() -- if null, the user is not signed in
 	const { userId } = await auth();
+
+	// Protect the route by checking if the user is signed in
 	if (!userId) {
-    	return <div>Access denied</div>;
-  	}
+		return <div>Sign in to view this page</div>
+	}
+	  // Get the Backend API User object when you need access to the user's information
+  	const user = await currentUser();
+
 
 	return (
 		<div className="p-6 space-y-4 flex flex-col items-center justify-center">
 			<h1 className="text-3xl font-bold text-center">Hard Charger App</h1>
-			<h2 className="text-2xl font-bold text-center">pick to win big!</h2>
+			<h2 className="text-2xl font-bold text-center">make some picks {user?.firstName?.toLowerCase()}!</h2>
 			<Link
 				href="/events"
 				className="flex justify-center mt-4 px-8 py-2 bg-blue-600 text-white hover:bg-blue-700 transition rounded-full shadow-md w-fit"
