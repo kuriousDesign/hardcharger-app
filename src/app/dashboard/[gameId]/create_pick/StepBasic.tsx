e91dbb8fb52718b078a9cadab4aaf67bffdd68dc
kuriousdesign <gardner.761@gmail.com>
2025-06-04 04:21:57 -0700

    fixed some bugs

--- src/app/dashboard/[gameId]/create_pick/StepBasic.tsx
+++ src/app/dashboard/[gameId]/create_pick/StepBasic.tsx
@@ -1,7 +1,6 @@
 // basic data like your name and pick nickname
 import { PickClientType } from '@/models/Pick';
 import { getRandomNicknameSuggestion } from './nicknameSuggestions';
-import { useEffect, useState } from 'react';
 import { BiRefresh } from 'react-icons/bi';
 
 
@@ -9,16 +8,16 @@ export default function StepBasic({ pickForm, setPickForm }: {
   pickForm: PickClientType;
   setPickForm: React.Dispatch<React.SetStateAction<PickClientType>>;
 }) {
-  const [nicknameSuggestion, setNicknameSuggestion] = useState("");
+  //const [nicknameSuggestion, setNicknameSuggestion] = useState("");
 
-  useEffect(() => {
+
     const loadSuggestion = async () => {
       const suggestion = await getRandomNicknameSuggestion();
-      setNicknameSuggestion(suggestion);
+      //setNicknameSuggestion(suggestion);
       setPickForm(prev => ({ ...prev, nickname: suggestion }));
     };
-    loadSuggestion();
-  }, []);
+
+
   return (
     <div className="space-y-4  w-full">
       <div>
@@ -38,10 +37,10 @@ export default function StepBasic({ pickForm, setPickForm }: {
             value={pickForm.nickname}
             onChange={(e) => setPickForm(prev => ({ ...prev, nickname: e.target.value }))}
             className="border p-2 w-full rounded"
-            placeholder={nicknameSuggestion}
+            placeholder={'funny nickname for your pick'}
           />
           <button
-            onClick={async () => setNicknameSuggestion(await getRandomNicknameSuggestion())}
+            onClick={loadSuggestion}
             className="p-2 rounded-full hover:bg-gray-100 border"
             type="button"
           >
