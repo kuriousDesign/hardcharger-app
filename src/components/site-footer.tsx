12854b81bd8310ac27795edf90ca3921669990de
kuriousdesign <gardner.761@gmail.com>
2025-06-20 00:29:04 -0700

    hiding footer

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -1,11 +1,11 @@
-import { siteConfig } from "@/lib/config"
+//import { siteConfig } from "@/lib/config"
 
 export function SiteFooter() {
   return (
     <footer className="group-has-[.section-soft]/body:bg-surface/40 3xl:fixed:bg-transparent dark:bg-transparent">
       <div className="container-wrapper px-4 xl:px-6">
         <div className="flex h-(--footer-height) items-center justify-between">
-          <div className="text-muted-foreground w-full text-center text-xs leading-loose sm:text-sm">
+          {/* <div className=" text-muted-foreground w-full text-center text-xs leading-loose sm:text-sm">
             Built by{" "}
             <a
               href={siteConfig.links.kurious}
@@ -25,7 +25,7 @@ export function SiteFooter() {
               GitHub
             </a>
             .
-          </div>
+          </div> */}
         </div>
       </div>
     </footer>
